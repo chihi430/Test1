b74bb1846b4923f480b3268ae48c62bf5fc157af
jms1004say <jms1004say@gmail.com>
2019-07-04 16:38:29 +0900

    주석추가

--- K_BitShiftOp.java
+++ K_BitShiftOp.java
@@ -3,10 +3,10 @@ public class K_BitShiftOp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num1 = 2;
-		int num2 = 3;
+		int num1 = 2; // num1 에대한 변수
+		int num2 = 3; // num2 에대한 변수
 		
-		System.out.println(num1+nmu3);
+		System.out.println(num1+nmu3); // 값 합산
 	}
 
 }
