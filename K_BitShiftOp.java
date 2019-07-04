f22674269635bc9706e7098baee2fee9c9874fff
jms1004say <jms1004say@gmail.com>
2019-07-04 16:46:29 +0900

    라인별 주석 추가

--- K_BitShiftOp.java
+++ K_BitShiftOp.java
@@ -3,13 +3,14 @@ public class K_BitShiftOp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num1 = 2;
-		int num2 = 3;
-		int num3 = 4;
-		int num4 = 5;
+		int num1 = 2; //주석추가
+		int num2 = 3; //주석추가 
+		int num3 = 4; //주석추가
+		int num4 = 5; //주석추가
 		
-		System.out.println(num1+nmu3);
-		System.out.println(num2+nmu4);
+		
+		System.out.println(num1+nmu3); //주석추가
+		System.out.println(num2+nmu4); //주석추가
 	}
 
 }
